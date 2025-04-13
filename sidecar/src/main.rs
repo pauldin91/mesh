151a457d622d878281c5422776ea2762f53b5e52
pauldin91@gmail.com <pauldin91@gmail.com>
2025-04-13 09:09:41 +0300

    fix

--- sidecar/src/main.rs
+++ sidecar/src/main.rs
@@ -21,19 +21,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
 
     let listener = TcpListener::bind(addr).await?;
 
-    // We start a loop to continuously accept incoming connections
     loop {
         let (stream, _) = listener.accept().await?;
 
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
         let io = TokioIo::new(stream);
 
-        // Spawn a tokio task to serve multiple connections concurrently
         tokio::task::spawn(async move {
-            // Finally, we bind the incoming connection to our `hello` service
             if let Err(err) = http1::Builder::new()
-                // `service_fn` converts our function in a `Service`
                 .serve_connection(io, service_fn(hello))
                 .await
             {
