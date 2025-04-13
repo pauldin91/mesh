b771e558360a9b5e69116a28890548ce0e0aa771
pauldin91@gmail.com <pauldin91@gmail.com>
2025-04-13 09:08:37 +0300

    basic server impl

--- sidecar/src/main.rs
+++ sidecar/src/main.rs
@@ -1,10 +1,52 @@
 use dotenvy::dotenv;
+use http_body_util::Full;
+use hyper::body::{Body, Bytes};
+use hyper::server::conn::http1;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
 use std::env;
+use std::net::SocketAddr;
+use hyper::service::{ service_fn,};
+use std::convert::Infallible;
+
+
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
     dotenv().ok();
-    let address = env::var("PORT").unwrap_or_else(|_|"4443".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_|"4443".to_string()).parse::<u16>().unwrap();
+    let addr = SocketAddr::from(([127, 0, 0, 1],port ));
+    
 
-    let server = Server::bind(&address).serve();
+    let listener = TcpListener::bind(addr).await?;
+
+    // We start a loop to continuously accept incoming connections
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+   
 }
+
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    println!("NEW REQUEST:");
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+}
+
+
