81a02ba773ef643006758180cbc37e56efdf93e8
pauldin91@gmail.com <pauldin91@gmail.com>
2025-04-14 07:10:06 +0300

    fix and track dockerfiles

---
+++ transaction_service/src/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/pauldin91/common/web"
+	"github.com/pauldin91/mesh/transaction_service/src/api"
+)
+
+func main() {
+
+	s := web.NewBuilder(func() *web.HttpApplication {
+		return &web.HttpApplication{}
+	})
+
+	cfgDir := "../../transaction_service"
+	httpServer := s.
+		WithConfig(cfgDir).
+		WithServer(api.GetRoutes()).
+		Build()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	httpServer.Start()
+	httpServer.WaitForShutdown(ctx)
+}
