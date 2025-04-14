06caa9fa839a2e8c0a0e19331884b151ba1144e4
pauldin91@gmail.com <pauldin91@gmail.com>
2025-04-14 17:59:34 +0300

    add reverse proxying

--- sidecar/src/main.rs
+++ sidecar/src/main.rs
@@ -1,24 +1,30 @@
 use dotenvy::from_filename;
 use http_body_util::{BodyExt, Full};
-use hyper::{Request, Response};
 use hyper::body::{Bytes, Incoming};
 use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response, Uri};
 use hyper_util::rt::TokioIo;
-use tokio::net::{TcpListener, TcpStream};
 use reqwest::Client;
+use std::collections::HashMap;
+use std::convert::Infallible;
 use std::env;
 use std::net::SocketAddr;
-use hyper::service::{service_fn};
-use std::convert::Infallible;
+use tokio::net::{TcpListener, TcpStream};
 
+use config::Config;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    from_filename("app.env").ok();
-    
-    let port = env::var("PORT").unwrap_or_else(|_| "3443".to_string()).parse::<u16>().unwrap();
+    dotenvy::from_filename("app.env").ok();
+
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "3443".to_string())
+        .parse::<u16>()
+        .unwrap();
+
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
-    
+
     let listener = TcpListener::bind(addr).await?;
 
     loop {
@@ -38,41 +44,75 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 }
 
 pub async fn proxy_handler(req: Request<Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    let remote_url = env::var("HOST_SRV_A").expect("HOST_SRV_A must be set");
-    let full_url = format!(
-        "{}{}",
-        remote_url,
-        req.uri().path_and_query().map(|x| x.as_str()).unwrap_or("/")
+    let mut settings = HashMap::<String, String>::new();
+    settings.insert(
+        "document_service".to_string(),
+        "https://localhost:7443".to_string(),
+    );
+    settings.insert(
+        "transaction_service".to_string(),
+        "https://localhost:8443".to_string(),
     );
 
-    let method = req.method().clone();
-    let headers = req.headers().clone();
+    println!("{}", req.uri());
 
-    let collected = req.into_body().collect().await.unwrap();
-    let body_bytes = collected.to_bytes();
+    let path_and_query = req.uri().to_string();
 
-    let client = Client::builder()
-        .danger_accept_invalid_certs(true)
-        .build()
-        .unwrap();
+    let srv = path_and_query.trim_start_matches("/").split("/").next();
 
-    let mut request_builder = client.request(method, full_url);
-    for (key, value) in headers.iter() {
-        if key != "host" { 
-            request_builder = request_builder.header(key, value);
-        }
+    let (srv_path, remote_host) = settings.get_key_value(srv.unwrap()).unwrap();
+
+    let path_and_query = path_and_query.trim_start_matches("/").replace(srv_path, "");
+
+    let remote_url = Uri::builder()
+        .authority(remote_host.as_str())
+        .path_and_query(path_and_query.as_str())
+        .build();
+
+    match remote_url {
+        Ok(url) => {
+            let full_url = format!(
+                "{}{}",
+                url,
+                req.uri()
+                    .path_and_query()
+                    .map(|x| x.as_str())
+                    .unwrap_or("/")
+            );
+
+            let method = req.method().clone();
+            let headers = req.headers().clone();
+
+            let collected = req.into_body().collect().await.unwrap();
+            let body_bytes = collected.to_bytes();
+
+            let client = Client::builder()
+                .danger_accept_invalid_certs(true)
+                .build()
+                .unwrap();
+
+            let mut request_builder = client.request(method, full_url);
+            for (key, value) in headers.iter() {
+                if key != "host" {
+                    request_builder = request_builder.header(key, value);
+                }
+            }
+
+            let response = request_builder.body(body_bytes).send().await.unwrap();
+
+            let status = response.status();
+            let response_headers = response.headers().clone();
+            let body = response.bytes().await.unwrap();
+
+            let mut builder = Response::builder().status(status);
+            for (key, value) in response_headers.iter() {
+                builder = builder.header(key, value);
+            }
+
+            Ok(builder.body(Full::new(Bytes::from(body))).unwrap())
+        },
+        Err(err) => {
+            panic!("{}",err)
+        },
     }
-
-    let response = request_builder.body(body_bytes).send().await.unwrap();
-
-    let status = response.status();
-    let response_headers = response.headers().clone();
-    let body = response.bytes().await.unwrap();
-
-    let mut builder = Response::builder().status(status);
-    for (key, value) in response_headers.iter() {
-        builder = builder.header(key, value);
-    }
-
-    Ok(builder.body(Full::new(Bytes::from(body))).unwrap())
 }
